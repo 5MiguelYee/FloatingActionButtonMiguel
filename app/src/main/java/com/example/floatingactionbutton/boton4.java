0b9389f1233c80fab8c219bd713d2313797f8609
rogersant0s04 <rogersant0s04@users.github.com>
2019-05-16 22:25:50 -0500

    commit 3

--- app/src/main/java/com/example/floatingactionbutton/boton4.java
+++ app/src/main/java/com/example/floatingactionbutton/boton4.java
@@ -1,13 +1,43 @@
 package com.example.floatingactionbutton;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 
 public class boton4 extends AppCompatActivity {
+
+    boolean click = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boton4);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.plus);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click = !click;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(),
+                            android.R.interpolator.fast_out_slow_in);
+
+                    view.animate()
+                            .rotation(click ? 45f : 0)
+                            .setInterpolator(interpolador)
+                            .start();
+                }
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/floatingactionbutton/boton5.java
+++ app/src/main/java/com/example/floatingactionbutton/boton5.java
@@ -2,12 +2,65 @@ package com.example.floatingactionbutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class boton5 extends AppCompatActivity {
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 
+public class boton5 extends AppCompatActivity implements View.OnClickListener {
+
+    private FABToolbarLayout morph; private FABToolbarLayout layout;
+    private View one, two, three, four;
+    private ListView list;
+    private View fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boton5);
+        layout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        list = (ListView) findViewById(R.id.list);
+        fab = findViewById(R.id.fabtoolbar_fab);
+
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+
+        String[] data = new String[30];
+        for(int i = 0; i < 30; i++) {
+            data[i] = "Test " + i;
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                layout.show();
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        layout.hide();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Element clicked", Toast.LENGTH_SHORT).show();
     }
 }
